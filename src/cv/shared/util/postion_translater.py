c556c5234ab9b27d9f1a3445509353f62773e68f
Nick Konecny <konecnyna@gmail.com>
2024-07-12 20:25:15 -0400

    updoot

--- src/cv/shared/util/postion_translater.py
+++ src/cv/shared/util/postion_translater.py
@@ -14,11 +14,11 @@ def translate_img_coordinates(event_x, event_y):
     return (inverted_x, translated_y)
 
 def translate_img_coordinates_scaled(event_x, event_y, scale):
-    box_width, box_height = 1920, 1080
+    box_width, box_height = 1280, 720
 
     #print(f"x: {event_x} y: {event_y} scale : {scale}" )
     
-    normalized_scale = 1
+    normalized_scale =  1 - scale
     
     translated_x = int(event_x  * box_width)
     translated_y = int(event_y  * box_height)
