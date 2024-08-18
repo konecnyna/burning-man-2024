9563dc48c21ba6e733966a97e02a1ca9e8702966
Nick Konecny <konecnyna@gmail.com>
2024-08-18 15:37:08 -0400

    updoot

--- src/server/server.js
+++ src/server/server.js
@@ -33,7 +33,7 @@ const productionState = {
   }
 }
 
-const stateManager = new StateManager(io, productionState)
+const stateManager = new StateManager(io, debugState)
 
 const eventManager = new EventManager(stateManager, io)
 const openCvEventBus = new OpenCvEventBus(io, stateManager.state)
