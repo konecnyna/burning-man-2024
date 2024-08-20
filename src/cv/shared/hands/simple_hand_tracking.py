0f819bdb6dc9fa882136d26c2fbf5d06011bccdc
Nick Konecny <konecnyna@gmail.com>
2024-08-19 20:24:10 -0400

    updoot

--- src/cv/shared/hands/simple_hand_tracking.py
+++ src/cv/shared/hands/simple_hand_tracking.py
@@ -15,8 +15,8 @@ class SimpleHandTracking:
         self.ws_client = ws_client
         self.hands = mp_hands.Hands(
             max_num_hands=4,
-            min_detection_confidence=0.1,
-            min_tracking_confidence=0.1,
+            min_detection_confidence=0.4,
+            min_tracking_confidence=0.2,
             model_complexity=0,
         )
         
